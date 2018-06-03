de11f2154fea27bf610a30bade1af93da69064fd
DESKTOP-LESU3Q0\jae <jh33836782@gmail.com>
2018-06-04 06:10:41 +0900

    Add[IDatabase] : 데이터베이스 연결 종료 인터페이스 추가
    데이터베이스 연결 및 종료 시 필요한 기능 메소드 인터페이스로 추가
    인터페이스 상속 받아서 데이터베이스 DAO 파일에서 메소드 구현

--- SignUpProgram/SignUpProgram/Etc/Database.cs
+++ SignUpProgram/SignUpProgram/Etc/Database.cs
@@ -5,10 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Data;
+using SignUpProgram.Etc;
 
 namespace SignUpProgram
 {
-    class Database
+    class Database : IDatabase
     {
         private static Database database;
         MemberVO Member = new MemberVO();
@@ -44,9 +45,7 @@ namespace SignUpProgram
         {
             string sql = "select * from member where " + attribute + " = '" + data + "';";
 
-            connect = new MySqlConnection(databaseConnect);
-            connect.Open();
-            command = new MySqlCommand(sql, connect);
+            this.ConnectDatabase(sql);
             reader = command.ExecuteReader();
 
             while (reader.Read())
@@ -62,7 +61,7 @@ namespace SignUpProgram
                 Member.MemberSPECIFICADDRESS = reader["specificaddress"].ToString();
             }
             reader.Close();
-            connect.Close();
+            DisconnectDatabase();
             return Member;
         }
         //회원정보 데이터베이스에 삽입 / 회원가입
@@ -72,22 +71,18 @@ namespace SignUpProgram
                 + "', '" + Member.MemberUSERNAME + "', '" + Member.MemberEMAIL + "', '" + Member.MemberPASSWORD + "', '" + Member.MemberPHONENUMBER
                 + "', '" + Member.MemberADDRESS + "', '" + Member.MemberSPECIFICADDRESS + "');";
 
-            connect = new MySqlConnection(databaseConnect);
-            connect.Open();
-            command = new MySqlCommand(sql, connect);
+            ConnectDatabase(sql);
             command.ExecuteNonQuery();
-            connect.Close();
+            DisconnectDatabase();
         }
         //데이터베이스에서 회원정보 삭제 / 탈퇴
         public void DeleteMember(MemberVO Member)
         {
             string sql = "delete from member where username = '" + Member.MemberUSERNAME + "';";
 
-            connect = new MySqlConnection(databaseConnect);
-            connect.Open();
-            command = new MySqlCommand(sql, connect);
+            ConnectDatabase(sql);
             command.ExecuteNonQuery();
-            connect.Close();
+            DisconnectDatabase();
         }
         //회원정보 데이터베이스에서 업데이트 / 회원정보 수정
         public void UpdateMember(MemberVO Member)
@@ -95,13 +90,31 @@ namespace SignUpProgram
             string sql = "update member set name = '" + Member.MemberNAME + "', email = '" + Member.MemberEMAIL + "', password = '" + Member.MemberPASSWORD
                 + "', address = '" + Member.MemberADDRESS + "', specificaddress = '" + Member.MemberSPECIFICADDRESS + "' where username = '" + Member.MemberUSERNAME + "';";
 
+            ConnectDatabase(sql);
+            command.ExecuteNonQuery();
+            DisconnectDatabase();
+        }
+
+        public string this[MemberVO member]
+        {
+            get
+            {
+                return member.MemberNAME;
+            }
+        }
+
+        //interface IDatabase.ConnectDatabase 메소드 구현
+        public void ConnectDatabase(string sql)
+        {
             connect = new MySqlConnection(databaseConnect);
             connect.Open();
             command = new MySqlCommand(sql, connect);
-            command.ExecuteNonQuery();
-            connect.Close();
         }
 
-
+        //interface IDatabase.DisconnectDatabase 메소드 구현
+        public void DisconnectDatabase()
+        {
+            connect.Close();
+        }
     }
 }
